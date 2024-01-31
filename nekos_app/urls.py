339c7d69e7eb45d463c93b5815b3872a7dd63b2d
Vinermy <egorkosachev@vk.com>
2024-01-31 22:02:53 +0700

    Added the index page

--- nekos_app/urls.py
+++ nekos_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from nekos_app import views
 
 urlpatterns = [
-    path('index', views.index, name='index')
+    path('index', views.index, name='index'),
+    path('image/{id:int}', views.image, name='image'),
+    path('random_girl', views.random_girl, name='random_girl')
 ]
--- nekos_app/views.py
+++ nekos_app/views.py
@@ -1,6 +1,37 @@
+from pprint import pprint
+
 from django.http import HttpRequest
 from django.shortcuts import render
+import requests as req
 
 # Create your views here.
 def index(request: HttpRequest):
-    return render(request, "index.html")
+    random_girls = req.get("https://api.nekosapi.com/v3/images/random",
+                           params={
+                               "rating": ["safe", "suggestive"],
+                               "limit": 5
+                           }).json()
+    ctx = {
+        "random_girls": random_girls
+    }
+    return render(request, "index.html", random_girls)
+
+
+def random_girl(request: HttpRequest):
+    image = req.get("https://api.nekosapi.com/v3/images/random",
+                    params={
+                        "rating": ["safe"],
+                        "limit": 1
+                    }).json()['items'][0]
+
+    ctx = {
+        "thumbnail_url": image['sample_url'],
+        "tags": [tag['name'] for tag in image['tags']],
+        "characters": ", ".join([character['name'] for character in image['characters']]) if image['characters'] else None,
+        "artist": image['artist']['name'] if image['artist'] is not None else None
+    }
+    return render(request, "blocks/image_card.html", ctx)
+
+
+def image(request: HttpRequest, id: int):
+    return
