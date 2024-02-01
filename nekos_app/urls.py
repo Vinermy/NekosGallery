7791b8fd0f4ebebb2848c5d9cd3306976ba9fb82
Vinermy <egorkosachev@vk.com>
2024-02-01 15:04:49 +0700

    Added the page for displaying single image with info

--- nekos_app/urls.py
+++ nekos_app/urls.py
@@ -4,6 +4,8 @@ from nekos_app import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
-    path('image/{id:int}', views.image, name='image'),
-    path('random_girl', views.random_girl, name='random_girl')
+    path('image/<int:id>', views.image, name='image'),
+    path('random_girl', views.random_girl, name='random_girl'),
+    path('character_arts/<int:id>', views.character_arts, name='character_arts'),
+    path('search', views.search, name='search')
 ]
--- nekos_app/views.py
+++ nekos_app/views.py
@@ -28,10 +28,36 @@ def random_girl(request: HttpRequest):
         "thumbnail_url": image['sample_url'],
         "tags": [tag['name'] for tag in image['tags']],
         "characters": ", ".join([character['name'] for character in image['characters']]) if image['characters'] else None,
-        "artist": image['artist']['name'] if image['artist'] is not None else None
+        "artist": image['artist']['name'] if image['artist'] is not None else None,
+        "id": image["id"]
     }
     return render(request, "blocks/image_card.html", ctx)
 
 
 def image(request: HttpRequest, id: int):
-    return
+    image = req.get(f"https://api.nekosapi.com/v3/images/{id}").json()
+
+    ctx = {
+        "id": image['id'],
+        "char_id": image['characters'][0]['id'] if image['characters'] else -1,
+        "image": image['image_url'],
+        "tags": [tag['name'] for tag in image['tags']],
+        "artist": image['artist']['name'] if image['artist'] is not None else None,
+        "characters": image['characters'][0]['name'] if image['characters'] else None,
+    }
+    return render(request, "image.html", ctx)
+
+def character_arts(request, id):
+    data = req.get(f"https://api.nekosapi.com/v3/images",
+                   params={
+                       "character": [id],
+                       "limit": 10
+                   }).json()
+
+    ctx = {
+        "images": [image['sample_url'] for image in data['items']]
+    }
+    return render(request, "blocks/character_arts.html", ctx)
+
+def search(request: HttpRequest):
+    return render(request, "search.html")
